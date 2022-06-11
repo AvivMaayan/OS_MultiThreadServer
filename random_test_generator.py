11a47374eb03ad77a18ab814cb20d36d75aded53
NitaiKlu <nitai.kluger@gmail.com>
2022-06-11 16:35:40 +0000

    random test

--- random_test_generator.py
+++ random_test_generator.py
@@ -1,51 +1,56 @@
-# to be written 
+# to be written
 # young grasshopper
 from random import seed
 from random import randint
+import os
 
-def index_command(i): #returns "index$i.html"
+
+def index_command(i):  # returns "index$i.html"
     res = "index"
     res += i
     res += ".html"
     return res
 
-# returns randomly 1,2,3. 
+# returns randomly 1,2,3.
 # 1 for html, 2 for sleep, 3 for doesn't exist
+
+
 def command_type():
-    seed(1)
-    return randint(0,3)
+    return randint(1, 3)
+
 
 def random_sleep():
-    seed(1)
-    return randint(0,20)
+    return randint(1, 20)
 
-#example: ./main 30 2010 
-#generates 30 random commands for port 2010
-def main(comm, argv):
-    first_half = "./client localhost "
-    sleep_comm = " output.cgi?"
-    f = open("test_file.txt", 'rw')
-    if(comm > 0):
-        num_of_comm = comm
-        port = argv
+
+
+# generates <num_of_comm> random commands 
+sleep_comm = " output.cgi?"
+if(os.path.isfile("test_me.txt")):
+    f = os.open("test_me.txt", os.O_RDWR | os.O_TRUNC)
+
+else :
+    f = os.open("test_me.txt", os.O_RDWR | os.O_CREAT)
+
+num_of_comm = 30
+
+final = ""
+for i in list(range(num_of_comm)):
+    command = str(command_type())
+    final = ""
+    if command == '1':  # html
+        final += " index"
+        final += str(command_type())
+        final += ".html"
+
+    elif command == '2':  # sleep
+        final += sleep_comm
+        final += str(random_sleep())
+
     else:
-        num_of_comm = 30
+        final += " doesn't exist"
+    final_bytes = str.encode(final)
+    os.write(f, final_bytes)
+    os.write(f, str.encode("\n"))
 
-    for i in list(range(1,num_of_comm)):
-        command = command_type()   
-        final = first_half 
-        final += port 
-        if command == 1: #html
-            final += " index"
-            final += command_type()
-            final += ".html"
-
-        elif command == 2: #sleep
-            final += sleep_comm
-            final += random_sleep()
-            
-        else:
-            final += " doesn't exist"
-
-        f.write(final)
-        f.write("\n")
+os.close(f)
